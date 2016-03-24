3edc8419e7d28f49f58442d0cc576580a840e661
Nicholas Muesch <mueschn@gmail.com>
2016-03-24 17:50:49 -0400

    Starts Beta Hanto

---
+++ BetaHanto/src/hanto/studentxxx/common/HantoBoard.java
@@ -0,0 +1,59 @@
+package hanto.studentxxx.common;
+
+import java.util.HashMap;
+
+import hanto.common.HantoCoordinate;
+import hanto.common.HantoPiece;
+
+public class HantoBoard {
+
+	private static HashMap<HantoCoordinateImpl, HantoPiece> currentBoard = new HashMap<HantoCoordinateImpl, HantoPiece>();
+
+	public static void AddPieceToBoard(HantoPiece piece, HantoCoordinate coordinate)
+	{
+		HantoCoordinateImpl newCord = new HantoCoordinateImpl(coordinate);
+		currentBoard.put(newCord, piece);
+	}
+	
+	public static HantoCoordinateImpl DistanceTo(HantoCoordinate from, HantoCoordinate to) {
+		int fromX = Math.abs(from.getX());
+		int fromY = Math.abs(from.getY());
+		int toX = Math.abs(to.getX());
+		int toY = Math.abs(to.getY());
+		return new HantoCoordinateImpl(Math.abs(fromX - toX), Math.abs(fromY - toY));
+	}
+
+	public static boolean isAdjacent(HantoCoordinate from, HantoCoordinate to) {
+		HantoCoordinateImpl dist = DistanceTo(from, to);
+		if (dist.getX() + dist.getY() != 1) {
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean isAdjacentToAny(HantoCoordinate coordinate) {
+		for (HantoCoordinate key : currentBoard.keySet()) {
+			if (isAdjacent(key, coordinate)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static boolean isTaken(HantoCoordinate coordinate) {
+		if (currentBoard.containsKey(coordinate)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static void printBoard() {
+		for (HantoCoordinate key : currentBoard.keySet()) {
+			System.out.println(key.getX() + " " + key.getY());
+		}
+	}
+
+	public static void reset() {
+		currentBoard.clear();
+	}
+}
